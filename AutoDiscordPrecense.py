8180b493c1e0dc79497c8b0fc84c39cc39a683a6
Johanstphn <juanesbs2003@hotmail.com>
2023-01-21 19:26:09 -0500

    Añadiendo un programa que cambia el estado de discord cada 15 segundos

---
+++ AutoDiscordPrecense.py
@@ -0,0 +1,15 @@
+import time
+from pypresence import Presence
+
+client_id = '1065492597843427418'
+RPC = Presence(client_id)
+RPC.connect()
+list = ["a ","b ","c ","d ","e ","f ","g ","h ","i ","j ","k ","l ","m ","n ","o ","p ","q ","r ","s ","t ","u ","v ","w ","x ","y ","z "]
+
+now = 0
+while True:
+    RPC.update(state=list[now], details="esto lo hice con codigo python 🙂", start=int(time.time()))
+    time.sleep(15)
+    now += 1
+    if now == len(list):
+        now = 0
