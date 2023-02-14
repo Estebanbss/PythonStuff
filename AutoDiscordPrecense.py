35b00b10f2fa15ec2412283595db280f90ad8b0e
Johanstphn <juanesbs2003@hotmail.com>
2023-02-14 18:19:10 -0500

    Añadiendo un close a rcp

--- AutoDiscordPrecense.py
+++ AutoDiscordPrecense.py
@@ -4,12 +4,17 @@ from pypresence import Presence
 client_id = '1065492597843427418'
 RPC = Presence(client_id)
 RPC.connect()
-list = ["a ","b ","c ","d ","e ","f ","g ","h ","i ","j ","k ","l ","m ","n ","o ","p ","q ","r ","s ","t ","u ","v ","w ","x ","y ","z "]
 
+list = ["a ","b ","c ","d ","e ","f ","g ","h ","i ","j ","k ","l ","m ","n ","o ","p ","q ","r ","s ","t ","u ","v ","w ","x ","y ","z "]
 now = 0
+
 while True:
+    print("Updating presence with state:", list[now])
     RPC.update(state=list[now], details="esto lo hice con codigo python 🙂", start=int(time.time()))
     time.sleep(15)
     now += 1
     if now == len(list):
         now = 0
+
+RPC.close()
+
