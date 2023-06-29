2981b06c6cc59e4bbe71e3981fef0a2825d91b67
Esteban <juanesbs2003@hotmail.com>
2023-06-29 00:39:51 -0500

    Learning python

---
+++ 00_hello.py
@@ -0,0 +1,20 @@
+
+# Hi word, this is a comment
+
+"""this 
+is a multiline 
+comment
+"""
+
+# Print a string
+
+print("Hi word")
+
+# Check the data type
+
+print(type("Hi word")) # type: "str"
+print(type(4)) # type: "int"
+print(type(4.4)) # type: "float"
+print(type(False)) # type: "bool"
+print(type(1 + 2j)) # type: "complex"
+
